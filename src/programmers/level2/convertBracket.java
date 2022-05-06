52f83ee70594c9938e25de8bfbe6dc558ffe440a
DESKTOP-9JKNRLV\kim <jchj108@naver.com>
2022-05-06 16:50:38 +0900

    해시 전화번호부 접두사 찾기

---
+++ src/programmers/level2/convertBracket.java
@@ -0,0 +1,26 @@
+package programmers.level2;
+
+public class convertBracket {
+
+    public static void main(String[] args) {
+
+        String p = "(()())()";
+        String result = solution(p);
+
+        System.out.println(result);
+
+    }
+
+    public static String solution(String p) {
+        String answer = "";
+
+        if(p.equals("")) {
+            return "";
+        }
+
+
+
+
+        return answer;
+    }
+}

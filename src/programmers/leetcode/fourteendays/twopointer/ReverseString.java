e06ebea09780494a201e3b5ec56ca3047f475206
jin <jchj108@naver.com>
2022-05-19 00:35:15 +0900

    투포인터 문자열 뒤집기 time complextiy O(n)

---
+++ src/programmers/leetcode/fourteendays/twopointer/ReverseString.java
@@ -0,0 +1,18 @@
+package programmers.leetcode.fourteendays.twopointer;
+
+public class ReverseString {
+    public static void main(String[] args) {
+
+        reverseString("asdfe".toCharArray());
+    }
+
+    public static void reverseString(char[] s) {
+        // Reverse String with O(n) space complexity
+        for(int i = 0, r = s.length-1; i < s.length / 2; i++) {
+            char temp = s[i];
+            s[i] = s[r];
+            s[r] = temp;
+            r--;
+        }
+    }
+}

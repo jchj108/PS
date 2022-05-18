b7a29f3f458f0ea9d41feba2b647521bff92f90d
jin <jchj108@naver.com>
2022-05-19 01:56:47 +0900

    투포인터 RotateArray 문자열 회전

---
+++ src/programmers/leetcode/fourteendays/twopointer/medium/RotateArray.java
@@ -0,0 +1,34 @@
+package programmers.leetcode.fourteendays.twopointer.medium;
+
+public class RotateArray {
+    public static void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        if(k == 0) {
+            return;
+        }
+
+        int l = 0;
+        int r = nums.length;
+        int rotate = r - k;
+        int[] arr = nums.clone();
+
+        while (l < r) {
+            if(rotate >= nums.length) {
+                rotate = rotate % nums.length;
+            }
+
+            nums[l] = arr[rotate];
+
+            l++;
+            rotate++;
+        }
+        for(int i : nums) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5};
+        rotate(nums, 2);
+    }
+}

a7549256ec85daacdc4427d9e0eba1ada4686df9
jin <jchj108@naver.com>
2022-05-22 13:23:58 +0900

    투포인터 MiddleOfTheLinkedList

--- src/leetcode/fourteendays/twopointer/MiddleOfTheLinkedList.java
+++ src/leetcode/fourteendays/twopointer/MiddleOfTheLinkedList.java
@@ -1,35 +1,45 @@
 package leetcode.fourteendays.twopointer;
 
-public class MiddleOfTheLinkedList {
+class ListNode {
+    int val;
+    ListNode next;
 
-    static class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
+    ListNode() {
     }
 
-//    public static ListNode middleNode(ListNode head) {
-//
-//
-//    }
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+
+public class MiddleOfTheLinkedList {
+
+    public static ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+
+        return slow;
+
+}
 
     public static void main(String[] args) {
         ListNode listNode = new ListNode(1);
         listNode.next = new ListNode(2);
-        listNode.next = new ListNode(3);
-        listNode.next = new ListNode(4);
+        listNode.next.next = new ListNode(3);
+        listNode.next.next.next = new ListNode(4);
 
-//        middleNode(listNode);
+        middleNode((listNode));
     }
 }

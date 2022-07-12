ef2a2b60f4ec2e6a1195777451cd8777079b5c7a
jin <jchj108@naver.com>
2022-07-12 23:51:52 +0900

    투포인터 연속된 수의 합

---
+++ src/doit/algorithm/chater3/twopointer/SumOfNumbers_2018.java
@@ -0,0 +1,34 @@
+package doit.algorithm.chater3.twopointer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SumOfNumbers_2018 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int l = 1;
+        int r = 1;
+        int cnt = 1;
+        int sum = 1;
+        while (r < N) {
+            if (sum == N) {
+                cnt++;
+                r++;
+                sum += r;
+            } else if (sum > N) {
+                sum -= l;
+                l++;
+            } else {
+                r++;
+                sum += r;
+            }
+        }
+        System.out.println(cnt);
+
+
+    }
+}

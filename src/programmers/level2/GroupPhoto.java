54f51b56e61219d6d2107492a4be3ae51d5b618d
DESKTOP-9JKNRLV\kim <jchj108@naver.com>
2022-08-10 15:41:41 +0900

    카카오 단체사진찍기 (dfs 순열)

--- src/programmers/level2/GroupPhoto.java
+++ src/programmers/level2/GroupPhoto.java
@@ -1,20 +1,79 @@
 package programmers.level2;
 
+import java.util.Arrays;
+
 public class GroupPhoto {
 
-    static int N;
+    static char[] friends = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
+    static Character[] res = new Character[8];
+    static boolean[] visited = new boolean[8];
+    static int cnt;
+    static String[] d;
 
     public static void main(String[] args) {
-        N = 2;
         String[] data = {"N~F=0", "R~T>2"};
-
+        solution(1, data);
     }
 
     public static int solution(int n, String[] data) {
 
-        N = n;
+        d = data;
 
-        int answer = 0;
-        return answer;
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length(); j++) {
+                char ch = data[i].charAt(j);
+            }
+        }
+        dfs(0, 0);
+        return cnt;
+    }
+
+    private static void dfs(int idx, int depth) {
+
+        if (depth == friends.length) {
+            if (validate()) {
+                cnt++;
+                for (char c : res) {
+                    System.out.print(c);
+                }
+                System.out.println();
+            }
+
+            return;
+        }
+        for (int i = 0; i < friends.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                res[depth] = friends[i];
+                dfs(i, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    private static boolean validate() {
+        char op;
+        for (String s : d) {
+            int a = Arrays.asList(res).indexOf(s.charAt(0));
+            int b = Arrays.asList(res).indexOf(s.charAt(2));
+            int val = s.charAt(4) - '0';
+            int distance = Math.abs(a - b) - 1;
+            op = s.charAt(3);
+
+            if (op == '=') {
+                if (distance != 0) {
+                    return false;
+                }
+            } else if (op == '>') {
+                if (distance <= val) {
+                    return false;
+                }
+            } else {
+                if (distance >= val) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }

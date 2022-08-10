270252006f0d53b3be6b7763b8a16fe7bae97f91
DESKTOP-9JKNRLV\kim <jchj108@naver.com>
2022-08-10 13:06:42 +0900

    n과 m 1 백트래킹 (중복순열)

---
+++ src/programmers/level2/GroupPhoto.java
@@ -0,0 +1,20 @@
+package programmers.level2;
+
+public class GroupPhoto {
+
+    static int N;
+
+    public static void main(String[] args) {
+        N = 2;
+        String[] data = {"N~F=0", "R~T>2"};
+
+    }
+
+    public static int solution(int n, String[] data) {
+
+        N = n;
+
+        int answer = 0;
+        return answer;
+    }
+}
